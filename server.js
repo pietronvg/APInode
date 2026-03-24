6e420b2aa9de3bbd9133d199e5acb9f962b87436
Lab05_etec <Lab05_etec@etec>
2026-03-24 18:17:14 -0300

    atualizado com o array nos usuarios

--- server.js
+++ server.js
@@ -4,10 +4,32 @@
 import express, { response } from "express";
 
 const app = express();
-const porta = 3000;
+const porta = 8000;
 
 app.get("/",(request,response) => {
   response.send("Olá (^-^)")
+});
+
+app.get("/usuario",(request,response) => {
+  response.json({
+    id: 1,
+    nome: "Pipi",
+    profissao:"Amar minha namo"
+  });
+});
+app.get("/usuarios",(request,response) => {
+  response.json([
+    {
+    id: 2,
+    nome: "Manu",
+    profissao:"Amar seu namo"
+    },
+    {
+    id: 3,
+    nome: "Pedro",
+    profissao:"Viver"
+    }
+  ]);
 });
 
 app.listen(porta, (request,response)=>{
